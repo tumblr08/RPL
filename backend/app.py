d43a77b5c5cc84da0bfc4e8eb17378ba3745820d
tumblr08 <putrabagus08@students.unnes.ac.id>
2025-08-19 02:09:56 +0700

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -291,5 +291,6 @@ if __name__ == "__main__":
     for rule in app.url_map.iter_rules():
         print(f"  {rule.endpoint}: {rule.rule} -> {list(rule.methods)}")
     # port = int(os.getenv("PORT", 8080))
+    port = int(os.environ.get("PORT", 8080))
     # print(">>> Using PORT =", port)
-    app.run()
+    app.run(host="0.0.0.0", port=port, debug=True)
